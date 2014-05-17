9a55c96a1589867db4db8f80cd1bdb28be87a665
Krzysztof Klinikowski <kkszysiu@gmail.com>
2014-05-17 21:32:43 +0200

    Update setup.py

--- src/charlockholmes.h
+++ src/charlockholmes.h
@@ -3,7 +3,9 @@
 
 int charlockholmes_init_encoding();
 PyObject *charlockholmes_get_supported_encodings(PyObject *self);
-PyObject *charlockholmes_encoding_detect(PyObject *self, PyObject *args, PyObject *keywds);
-PyObject *charlockholmes_encoding_detect_all(PyObject *self, PyObject *args, PyObject *keywds);
+PyObject *charlockholmes_encoding_detect(PyObject *self, PyObject *args);
+PyObject *charlockholmes_encoding_detect_all(PyObject *self, PyObject *args);
+PyObject *charlockholmes_get_strip_tags(PyObject *self);
+PyObject *charlockholmes_set_strip_tags(PyObject *self, PyObject *args);
 
 #endif /* __CHARLOCK_HOLMES_H */
